0cfb700d30036f87d667fe9800447fab0414a686
mohammed jamal <yamni.zero@gmail.com>
2024-05-26 14:44:04 +0200

    Ui Card

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { fetchCars } from "@/utils";
 
 export default async function Home() {
     const allCars = await fetchCars();
+
     
     const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
