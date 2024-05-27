2620cc88cd2cdf7f8681208a8cfd550e5cebc893
mohammed jamal <yamni.zero@gmail.com>
2024-05-27 18:07:51 +0200

    -create Dialog

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,8 +1,69 @@
-import React from 'react'
 
-function CarDetails() {
+import Image from 'next/image';
+import { Fragment } from 'react';
+
+import { Dialog, Transition } from '@headlessui/react';
+
+
+import { CarProps } from '@/types';
+interface CarDetailsProps{
+    isOpen:boolean;
+    closeModal: () => void;
+    car:CarProps;
+}
+
+function CarDetails({ isOpen, closeModal ,car} : CarDetailsProps) {
   return (
-    <div>CarDetails</div>
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+          <Dialog as='div' className="relative z-10" onClose={closeModal} >
+              <Transition.Child
+               as={Fragment}
+               enter='ease-out duration-300'
+               enterFrom='opacity-0'
+               enterTo='opacity-100'
+               leave='ease-in duration-200'
+               leaveFrom='opacity-100'
+               leaveTo='opacity-0'
+               >
+                <div className='fixed inset-0 bg-black bg-opacity-25'>
+
+                </div>
+              </Transition.Child>
+
+              <div className='fixed inset-0 overflow-y-auto'>
+                  <div className='flex min-h-full items-center justify-center p-4 text-center'>
+                  <Transition.Child
+               as={Fragment}
+               enter='ease-out duration-300'
+               enterFrom='opacity-0 scale-95'
+               enterTo='opacity-100 scale-100'
+               leave='ease-in duration-200'
+               leaveFrom='opacity-100 scale-100'
+               leaveTo='opacity-0 scale-95'
+               >
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+                  <button 
+                    type='button'
+                    onClick={closeModal}
+                  > 
+                    <Image 
+                      src="/close.svg"
+                      alt='close'
+                      width={20}
+                      height={20}
+                      className='object-contain'
+                    >
+
+                    </Image>
+                  </button>
+                </Dialog.Panel>
+               </Transition.Child>
+                  </div>
+              </div>
+          </Dialog>
+      </Transition>
+    </>
   )
 }
 
